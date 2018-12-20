9774244201c5a722609c6330e40fb5bcbc1c18c4
Charly3X <kinovhod@gmail.com>
2018-12-20 11:33:07 +0200

    Update schema.rb

--- lib/csvlint/schema.rb
+++ lib/csvlint/schema.rb
@@ -74,7 +74,7 @@ module Csvlint
 
       found_header = header.to_csv(:row_sep => '')
       expected_header = @fields.map{ |f| f.name }.to_csv(:row_sep => '')
-      if found_header != expected_header
+      if found_header.to_s.upcase != expected_header.to_s.upcase
         build_warnings(:malformed_header, :schema, 1, nil, found_header, "expectedHeader" => expected_header)
       end
       return valid?
